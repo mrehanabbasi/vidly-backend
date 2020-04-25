83ab2eb0cb77f1278bc56f3705ccd7a78f0ec58f
MRA <mrehanabbasi@yahoo.com>
2020-04-25 18:42:08 +0500

    Connect genre routes to MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const express = require('express');
 const app = express();
+
+mongoose
+  .connect('mongodb://localhost/vidly', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch((err) => console.log('Could not connect to MongoDB...'));
 
 app.use(express.json());
 app.use('/api/genres', genres);
